0ea617f108800551ff60922e3e1436872ded725e
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2025-01-13 22:57:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,32 @@
-const bodyParser=require("body-parser")
-const express=require("express")
-const app=express();
-const path=require("path")
-const cors=require("cors");
-const {connect}=require("./db")
-const router=require("./Routes/index")
-const port =5000
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connect } = require("./db");
+const router = require("./Routes/index");
+const port = 5000;
 
-app.use(cors())
-app.use(bodyParser.json({limit:"50mb"}))
-app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
-app.use(express.json())
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Hello This is My backend")
-})
-app.use("/api",router)
+app.get("/", (req, res) => {
+  res.send("Hello This is My backend");
+});
+app.use("/api", router);
 connect();
-app.use((req,res,next)=>{
-    req.header("Access-Control-Allow-Origin","*")
-    res.header("Access-Control-Allow-Origin","*")
-    next()
- })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({ origin: 'https://internaura-app.vercel.app' }));
 
 app.get('/api/job', (req, res) => {
   res.json({ message: 'CORS issue fixed!' });
 });
-app.listen(port,()=>{
-    console.log("server is running on port ")
-})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
