d19d55c6e577a67b26ec9977336bef9f15258d54
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2025-01-13 19:39:18 +0530

    Update Home.jsx
    update url

--- frontend/src/Componets/Home/Home.jsx
+++ frontend/src/Componets/Home/Home.jsx
@@ -17,7 +17,7 @@ const [internshipData,setInternshipData]=useState([])
 useEffect(()=>{
     const fetchData= async()=>{
         try {
-        const response= await axios.get(`https://internshipbackend-vwja.onrender.com/api/internship`)
+        const response= await axios.get(`https://intern-aura.vercel.app/api/internship`)
         setInternshipData(response.data)
     } catch (error) {
            console.log(error) 
