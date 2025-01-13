253519817f06bf99247fa6532f04b5cbb8fc89d0
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2025-01-13 19:50:11 +0530

    Update UserApplicatiom.jsx

--- frontend/src/profile/UserApplicatiom.jsx
+++ frontend/src/profile/UserApplicatiom.jsx
@@ -15,7 +15,7 @@ function UserApplicatiom() {
     useEffect(()=>{
         const fetchApplication= async()=>{
         try {
-            const response=await axios.get("https://internshipbackend-vwja.onrender.com/api/application")
+            const response=await axios.get("https://intern-aura.vercel.app/api/application")
 setApplication(response.data)
 
         } catch (error) {
